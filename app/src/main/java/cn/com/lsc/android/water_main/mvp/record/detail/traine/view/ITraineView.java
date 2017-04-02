2eed35c0f2cea5e94c5c46b4d57a9c607a77bb25
V-JOY\liaoshucai <497054711@qq.com>
2017-04-02 18:53:48 +0800

    no message

--- app/src/main/java/cn/com/lsc/android/water_main/mvp/record/detail/traine/view/ITraineView.java
+++ app/src/main/java/cn/com/lsc/android/water_main/mvp/record/detail/traine/view/ITraineView.java
@@ -4,5 +4,5 @@ package cn.com.lsc.android.water_main.mvp.record.detail.traine.view;
  * Created by Administrator on 2017/3/31.
  */
 
-public interface IUserView {
+public interface ITraineView {
 }
